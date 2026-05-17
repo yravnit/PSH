c2eb6609a7aba0bc85d5958987f65367705d5675
Ravnit Yadav <yrovnit47@gmail.com>
2026-05-17 20:29:19 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,18 +2,50 @@ import sys
 
 
 def main():
+    builtins = ["echo", "exit", "type"]
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = sys.stdin.readline().rstrip("\n")
+        line = sys.stdin.readline()
+
+        if  not line:
+            break
+
+        line = line.rstrip("\n")
+        parts = line.split()
+
+        # Empty input
+        if len(parts) == 0:
+            continue
+
+        command = parts[0]
+
+        # exit
         if command == "exit":
             break
-        if command.startswith("echo"):
-            print(command[5:])
-            continue
-        print(f"{command}: command not found")
 
+        # echo
+        elif command == "echo":
+            sys.stdout.write(" ".join(parts[1:]) + "\n")
+            sys.stdout.flush()
+        
+        # type
+        elif command == "type":
+            target = parts[1]
+
+            if target in builtins:
+                sys.stdout.write(f"{target} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{target}: not found\n")
+
+            sys.stdout.flush()
+
+        # unknown command
+        else:
+            sys.stdout.write(f"{command}: command not found\n")
+            sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
