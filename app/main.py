11d8be11ff77ec4ef4db1fb69cb58a3d549c0b1f
Ravnit Yadav <yrovnit47@gmail.com>
2026-05-17 19:07:29 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    sys.stdout.flush()
+
+    command = sys.stdin.readline().rstrip("\n")
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
